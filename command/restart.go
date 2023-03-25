75f62676941268b3d6123697fc3069cf40c66196
e.kiru <evgeny.kiru@v6services.ru>
2023-03-25 23:42:00 +0300

    added restart

---
+++ command/restart.go
@@ -0,0 +1,38 @@
+package command
+
+import (
+	"backapper-client/params"
+	"backapper-client/pathresolver"
+	"io"
+	"log"
+	"net/http"
+)
+
+type Restart struct {
+}
+
+func (b *Restart) Run(p *params.Params) error {
+	response, err := http.Get(pathresolver.Resolve(p))
+	if err != nil {
+		return err
+	}
+
+	defer func(Body io.ReadCloser) {
+		err := Body.Close()
+		if err != nil {
+			log.Println(err.Error())
+		}
+	}(response.Body)
+
+	resp, err := io.ReadAll(response.Body)
+	if err != nil {
+		return err
+	}
+
+	log.Println(string(resp))
+	return nil
+}
+
+func (b *Restart) Type() string {
+	return "restart"
+}
