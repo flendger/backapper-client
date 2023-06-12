eb5d9875ee4f8bbd6eb4ce46b18ebea513047d0e
e.kiru <evgeny.kiru@v6services.ru>
2023-06-12 22:52:32 +0300

    refactoring

--- clientfactory/clientfactory.go
+++ clientfactory/clientfactory.go
@@ -2,7 +2,9 @@ package clientfactory
 
 import (
 	"backapper-client/profile"
+	"context"
 	"h12.io/socks"
+	"net"
 	"net/http"
 )
 
@@ -13,7 +15,11 @@ func CreateClient(p *profile.AppProfile) *http.Client {
 
 	if proxyHost != "" && proxyPort != "" {
 		dialSocksProxy := socks.Dial("socks5://" + proxyHost + ":" + proxyPort + "?timeout=72000s")
-		tr := &http.Transport{Dial: dialSocksProxy}
+
+		tr := &http.Transport{DialContext: func(ctx context.Context, network, addr string) (net.Conn, error) {
+			return dialSocksProxy(network, addr)
+		}}
+
 		return &http.Client{Transport: tr}
 	} else {
 		return &http.Client{}
