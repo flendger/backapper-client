0fdca34fc318714e66c180192fe5e0ed6e6cabd3
e.kiru <evgeny.kiru@v6services.ru>
2023-06-12 21:20:51 +0300

    added clientfactory

---
+++ clientfactory/clientfactory.go
@@ -0,0 +1,21 @@
+package clientfactory
+
+import (
+	"backapper-client/profile"
+	"h12.io/socks"
+	"net/http"
+)
+
+func CreateClient(p *profile.AppProfile) *http.Client {
+
+	proxyHost := p.Proxy.Host
+	proxyPort := p.Proxy.Port
+
+	if proxyHost != "" && proxyPort != "" {
+		dialSocksProxy := socks.Dial("socks5://" + proxyHost + ":" + proxyPort + "?timeout=5s")
+		tr := &http.Transport{Dial: dialSocksProxy}
+		return &http.Client{Transport: tr}
+	} else {
+		return &http.Client{}
+	}
+}
